46f5f93e0c9f9a6b1fb6a05c40a8654de1716e67
Simon Gockner <simon.gockner@gmail.com>
2019-06-04 22:31:28 +0200

    - add IInjectorInstaller

---
+++ LightweightIocContainer/Interfaces/IInjectorInstaller.cs
@@ -0,0 +1,20 @@
+﻿// Author: simon.gockner
+// Created: 2019-05-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using LightweightIocContainer.Interfaces.Registrations;
+
+namespace LightweightIocContainer.Interfaces
+{
+    /// <summary>
+    /// The base class for <see cref="IInjectorContainer"/> installers
+    /// </summary>
+    public interface IInjectorInstaller
+    {
+        /// <summary>
+        /// Install the needed <see cref="IRegistrationBase"/>s in the given <see cref="IInjectorContainer"/>
+        /// </summary>
+        /// <param name="container">The current <see cref="IInjectorContainer"/></param>
+        void Install(IInjectorContainer container);
+    }
+}
