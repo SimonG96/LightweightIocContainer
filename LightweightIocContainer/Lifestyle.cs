31419c65ceaf6d96a802e7402dd745a7412985bc
Simon Gockner <simon.gockner@gmail.com>
2019-06-04 22:31:52 +0200

    - add lifestyles

---
+++ LightweightIocContainer/Lifestyle.cs
@@ -0,0 +1,26 @@
+﻿// Author: simon.gockner
+// Created: 2019-05-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using LightweightIocContainer.Interfaces.Registrations;
+
+namespace LightweightIocContainer
+{
+    /// <summary>
+    /// The Lifestyles that can be used for a <see cref="IDefaultRegistration{TInterface}"/>
+    /// </summary>
+    public enum Lifestyle
+    {
+        /// <summary>
+        /// A new instance gets created every time an instance is resolved
+        /// </summary>
+        Transient,
+
+        /// <summary>
+        /// One instance is created that gets returned every time an instance is resolved
+        /// </summary>
+        Singleton
+
+        //TODO: Add Lifestyle.Multiton
+    }
+}
