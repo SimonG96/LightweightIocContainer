3c636a786eeac278f413e381a53c4315bb491b4f
Simon Gockner <simon.gockner@gmail.com>
2019-11-22 14:27:29 +0100

    #29: add unit test for withParameters method

--- Test.LightweightIocContainer/RegistrationBaseTest.cs
+++ Test.LightweightIocContainer/RegistrationBaseTest.cs
@@ -3,7 +3,9 @@
 // Copyright(c) 2019 SimonG. All Rights Reserved.
 
 using System;
+using JetBrains.Annotations;
 using LightweightIocContainer;
+using LightweightIocContainer.Exceptions;
 using LightweightIocContainer.Interfaces;
 using LightweightIocContainer.Interfaces.Registrations;
 using LightweightIocContainer.Registrations;
@@ -22,12 +24,36 @@ namespace Test.LightweightIocContainer
             void DoSomething();
         }
 
+        private interface IFoo
+        {
+
+        }
+
+        private interface IBar
+        {
+
+        }
+
         private class Test : ITest
         {
             public void DoSomething()
             {
                 throw new Exception();
             }
+        }
+
+        [UsedImplicitly]
+        private class Foo : IFoo
+        {
+            public Foo(IBar bar, ITest test)
+            {
+
+            }
+        }
+
+        private class Bar : IBar
+        {
+
         }
 
         #endregion
@@ -42,6 +68,52 @@ namespace Test.LightweightIocContainer
             ITest test = new Test();
             
             Assert.Throws<Exception>(() => testRegistration.OnCreateAction(test));
+        }
+
+        [Test]
+        public void TestWithParameters()
+        {
+            RegistrationFactory registrationFactory = new RegistrationFactory(new Mock<IIocContainer>().Object);
+
+            IBar bar = new Bar();
+            ITest test = new Test();
+
+            IRegistrationBase<IFoo> testRegistration = registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters(bar, test);
+
+            Assert.AreEqual(bar, testRegistration.Parameters[0]);
+            Assert.AreEqual(test, testRegistration.Parameters[1]);
+        }
+
+        [Test]
+        public void TestWithParametersDifferentOrder()
+        {
+            RegistrationFactory registrationFactory = new RegistrationFactory(new Mock<IIocContainer>().Object);
+
+            IBar bar = new Bar();
+            ITest test = new Test();
+
+            IRegistrationBase<IFoo> testRegistration = registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters((0, bar), (3, test), (2, "SomeString"));
+
+            Assert.AreEqual(bar, testRegistration.Parameters[0]);
+            Assert.IsInstanceOf<InternalResolvePlaceholder>(testRegistration.Parameters[1]);
+            Assert.AreEqual("SomeString", testRegistration.Parameters[2]);
+            Assert.AreEqual(test, testRegistration.Parameters[3]);
+        }
+
+        [Test]
+        public void TestWithParametersCalledTwice()
+        {
+            RegistrationFactory registrationFactory = new RegistrationFactory(new Mock<IIocContainer>().Object);
+            Assert.Throws<InvalidRegistrationException>(() => registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters(new Bar()).WithParameters(new Test()));
+            Assert.Throws<InvalidRegistrationException>(() => registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters((0, new Bar())).WithParameters((1, new Test())));
+        }
+
+        [Test]
+        public void TestWithParametersNoParametersGiven()
+        {
+            RegistrationFactory registrationFactory = new RegistrationFactory(new Mock<IIocContainer>().Object);
+            Assert.Throws<InvalidRegistrationException>(() => registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters((object[])null));
+            Assert.Throws<InvalidRegistrationException>(() => registrationFactory.Register<IFoo, Foo>(Lifestyle.Transient).WithParameters(((int index, object parameter)[])null));
         }
     }
 }
