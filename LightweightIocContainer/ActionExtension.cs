c25896a56e648e232076acb681773bd7f72f7e6a
Simon Gockner <simon.gockner@gmail.com>
2019-12-11 15:33:37 +0100

    #33: Add extension method to convert an action: Needed to handle OnCreateActions from inheriting types later on

---
+++ LightweightIocContainer/ActionExtension.cs
@@ -0,0 +1,26 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-12-11
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using System;
+
+namespace LightweightIocContainer
+{
+    internal static class ActionExtension
+    {
+        /// <summary>
+        /// Convert an <see cref="Action{T2}"/> to an <see cref="Action{T1}"/> of an inherited <see cref="Type"/>
+        /// </summary>
+        /// <typeparam name="T1">The <see cref="Type"/> of the <see cref="Action{T1}"/> to convert to, has to be implemented by <typeparamref name="T2"/></typeparam>
+        /// <typeparam name="T2">The <see cref="Type"/> of the given <see cref="Action{T2}"/>, has to implement <typeparamref name="T1"/></typeparam>
+        /// <param name="action">The given <see cref="Action{T2}"/> to convert</param>
+        /// <returns>An <see cref="Action{T1}"/> converted from the given <see cref="Action{T2}"/></returns>
+        public static Action<T1> Convert<T1, T2>(this Action<T2> action) where T1 : T2
+        {
+            if (action == null)
+                return null;
+
+            return t => action(t);
+        }
+    }
+}
---
+++ Test.LightweightIocContainer/ActionExtensionTest.cs
@@ -0,0 +1,52 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-12-11
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using System;
+using LightweightIocContainer;
+using NUnit.Framework;
+
+namespace Test.LightweightIocContainer
+{
+    [TestFixture]
+    public class ActionExtensionTest
+    {
+        #region TestClasses
+
+        private interface IBar
+        {
+            void Throw();
+        }
+
+        private interface IFoo : IBar
+        {
+
+        }
+
+        private class Foo : IFoo
+        {
+            public void Throw()
+            {
+                throw new Exception();
+            }
+        }
+
+        #endregion TestClasses
+
+        [Test]
+        public void TestConvert()
+        {
+            Action<IBar> barAction = bar => bar.Throw();
+            Action<IFoo> action = barAction.Convert<IFoo, IBar>();
+
+            Assert.Throws<Exception>(() => action(new Foo()));
+        }
+
+        [Test]
+        public void TestConvertActionNull()
+        {
+            Action<IBar> barAction = null;
+            Assert.Null(barAction.Convert<IFoo, IBar>());
+        }
+    }
+}
