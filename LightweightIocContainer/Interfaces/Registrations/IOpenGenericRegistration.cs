e816546eb2f201f31c3c4849169043df48d825c3
Simon G <simon.gockner@gmail.com>
2020-09-18 22:37:35 +0200

    - add OpenGenericRegistration

---
+++ LightweightIocContainer/Interfaces/Registrations/IOpenGenericRegistration.cs
@@ -0,0 +1,13 @@
+﻿// Author: Simon Gockner
+// Created: 2020-09-18
+// Copyright(c) 2020 SimonG. All Rights Reserved.
+
+using System;
+
+namespace LightweightIocContainer.Interfaces.Registrations
+{
+    public interface IOpenGenericRegistration : IRegistration, ILifestyleProvider
+    {
+        Type ImplementationType { get; }
+    }
+}
---
+++ LightweightIocContainer/Registrations/OpenGenericRegistration.cs
@@ -0,0 +1,26 @@
+﻿// Author: Simon Gockner
+// Created: 2020-09-18
+// Copyright(c) 2020 SimonG. All Rights Reserved.
+
+using System;
+using LightweightIocContainer.Interfaces.Registrations;
+
+namespace LightweightIocContainer.Registrations
+{
+    public class OpenGenericRegistration : IOpenGenericRegistration
+    {
+        public OpenGenericRegistration(Type interfaceType, Type implementationType, Lifestyle lifestyle)
+        {
+            InterfaceType = interfaceType;
+            ImplementationType = implementationType;
+            Lifestyle = lifestyle;
+            
+            Name = $"{InterfaceType.Name}, {ImplementationType.Name}, Lifestyle: {Lifestyle.ToString()}";
+        }
+        
+        public string Name { get; }
+        public Type InterfaceType { get; }
+        public Type ImplementationType { get; }
+        public Lifestyle Lifestyle { get; }
+    }
+}
