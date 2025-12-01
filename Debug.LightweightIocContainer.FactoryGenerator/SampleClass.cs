e7e347a477afe462a2632cb03a30db1845240a94
Simon G. <simon.gockner@gmail.com>
2025-12-01 13:52:59 +0100

    - add samle classes

---
+++ Debug.LightweightIocContainer.FactoryGenerator/SampleClass.cs
@@ -0,0 +1,13 @@
+﻿// Author: Simon.Gockner
+// Created: 2025-12-01
+// Copyright(c) 2025 SimonG. All Rights Reserved.
+
+namespace Debug.LightweightIocContainer.FactoryGenerator;
+
+public class SampleClass : ISampleClass;
+public interface ISampleClass;
+
+public interface ISampleClassFactory
+{
+    ISampleClass Create();
+}
