79642012e67893aa4a86e86de39f4eb741bf7d34
Simon G. <simon.gockner@gmail.com>
2025-12-03 09:04:58 +0100

    - add methods to test more factory types

--- Debug.LightweightIocContainer.FactoryGenerator/SampleClass.cs
+++ Debug.LightweightIocContainer.FactoryGenerator/SampleClass.cs
@@ -10,4 +10,10 @@ public interface ISampleClass;
 public interface ISampleClassFactory
 {
     ISampleClass Create();
+    T Create<T>() where T : ISampleClass;
+    Task<ISampleClass> CreateAsync();
+    
+    ISampleClass Create(string name);
+    T Create<T, U, V>(U param1, V param2) where T : ISampleClass where U : class, new() where V : struct;
+    Task<T> CreateAsync<T, U>(U parameter) where T : ISampleClass where U : class;
 }
