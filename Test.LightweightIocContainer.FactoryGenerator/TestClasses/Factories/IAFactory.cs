b16721164b9aa8a58bbcf79b9afda964616612a4
Simon G. <simon.gockner@gmail.com>
2025-12-03 15:02:53 +0100

    - remove unneeded annotations

--- Test.LightweightIocContainer.FactoryGenerator/TestClasses/Factories/IAFactory.cs
+++ Test.LightweightIocContainer.FactoryGenerator/TestClasses/Factories/IAFactory.cs
@@ -1,9 +1,7 @@
-﻿using JetBrains.Annotations;
-using Test.LightweightIocContainer.FactoryGenerator.TestClasses.Interfaces;
+﻿using Test.LightweightIocContainer.FactoryGenerator.TestClasses.Interfaces;
 
 namespace Test.LightweightIocContainer.FactoryGenerator.TestClasses.Factories;
 
-[UsedImplicitly]
 public interface IAFactory
 {
     IA Create();
--- Test.LightweightIocContainer.FactoryGenerator/TestClasses/TestByte.cs
+++ Test.LightweightIocContainer.FactoryGenerator/TestClasses/TestByte.cs
@@ -1,11 +1,9 @@
-﻿using JetBrains.Annotations;
-using Test.LightweightIocContainer.FactoryGenerator.TestClasses.Interfaces;
+﻿using Test.LightweightIocContainer.FactoryGenerator.TestClasses.Interfaces;
 
 namespace Test.LightweightIocContainer.FactoryGenerator.TestClasses;
 
 public class TestByte : IDefaultTest
 {
-    [UsedImplicitly]
     private readonly byte _id;
 
     public TestByte(byte id) => _id = id;
