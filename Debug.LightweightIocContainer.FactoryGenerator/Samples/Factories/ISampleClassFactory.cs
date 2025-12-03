610c47bc15011d3c886d19a15867153b5cb4545e
Simon G. <simon.gockner@gmail.com>
2025-12-03 16:05:01 +0100

    - add more test cases with nullable and generic parameters

--- Debug.LightweightIocContainer.FactoryGenerator/Samples/Factories/ISampleClassFactory.cs
+++ Debug.LightweightIocContainer.FactoryGenerator/Samples/Factories/ISampleClassFactory.cs
@@ -12,5 +12,10 @@ public interface ISampleClassFactory
     T Create<T, U, V>(U param1, V param2) where T : ISampleClass where U : class, new() where V : struct;
     Task<T> CreateAsync<T, U>(U parameter) where T : ISampleClass where U : class;
     
+    ISampleClass Create<T>(Task<T> task) where T : Task<ISampleClass>;
+    
+    Task<T?> Create<T>(ISampleClass? sampleClass) where T : class, ISampleClass;
+    ISampleClass? Create(int id);
+    
     void ClearMultitonInstance<T>() where T : ISampleClass;
 }
